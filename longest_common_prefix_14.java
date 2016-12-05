1930ab862a6a0d5afff2d7eae3f8aa2990e90ec6
yubin <qybbqybb@gmail.com>
2016-12-05 12:24:52 -0800

    yubin: add 14

---
+++ longest_common_prefix_14.java
@@ -0,0 +1,13 @@
+public String longestCommonPrefix(String[] strs){
+  if(strs==null||strs.length==0) return "";
+  String prefix =strs[0];
+  for(int i=1;i<strs.length;i++){
+    int j=0;
+    while(j<strs[i].length()&&j<prefix.length()&&strs[i].charAt(j)==prefix.charAt(j)){
+      j++
+    }
+    prefix=prefix.substring(0,j);
+  }
+
+  return prefix;
+}
